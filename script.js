80e6b33d0a734d2da4bf0fd98f48b37dd14f7e94
onurtalayhan <onur.talayhan@gmail.com>
2020-11-28 08:37:57 +0100

    array test

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const form = document.querySelector('form');
 const input = document.querySelector('#txtTaskName');
 const btnDeleteAll = document.querySelector('#btnDeleteAll');
 const taskList = document.querySelector('#task-list');
+const items = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+loadItems();
 
 addEventListener();
 
@@ -14,29 +17,47 @@ function addEventListener() {
     btnDeleteAll.addEventListener('click', deleteAll);
 }
 
-function addNewItem(e) {
-
-    if (input.value == 0) {
-        alert('add new item');
-    }
+function loadItems(){
+    items.forEach(function(item){
+        createItem(item);
+    })
+}
+function createItem(text){
+    //create li
     const li = document.createElement('li');
     li.className = 'list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(document.createTextNode(text));
 
+    // create a
     const a = document.createElement('a');
     a.classList = 'delete-item float-right';
     a.setAttribute('href', '#');
     a.innerHTML = '<i class="fas fa-times"></i>';
 
+    //add a to li
     li.appendChild(a);
-    taskList.appendChild(li);
 
+    //add li to ul
+    taskList.appendChild(li);
+}
+
+function addNewItem(e) {
+
+    if (input.value == 0) {
+        alert('add new item');
+    }
+    
+    // create item
+    createItem(input.value);
+    //clear input
+    // input.value = '';
     e.preventDefault();
 }
 
 function deleteItem(e) {
-    if (confirm('Are you sure?')) {
+    
         if (e.target.className === 'fas fa-times') {
+            if (confirm('Are you sure?')) {
             e.target.parentElement.parentElement.remove();
         }
         e.preventDefault();
